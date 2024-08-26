7e8803bc49c38acc488ecfbc1e3d6da16e9a84b9
mahdieh <madidgn82@gmail.com>
2024-08-26 16:51:06 +0330

    style footer

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
             }}
             navigation={true}
             modules={[Autoplay, Navigation]}
-            className="mySwiper"
+            className={styles.mySwiper}
           >
             <SwiperSlide>
               <img src="./slide1.avif" alt="slide" />
@@ -108,42 +108,177 @@ function App() {
               <img src="./slide4.avif" alt="slide" />
             </SwiperSlide>
           </Swiper>
-          <div className={styles.products}>
-            <p>
-              You are on amazon.com. You can also shop on Amazon Iran for
-              millions of products with fast local delivery. Click here to go to
-              amazon.de
-            </p>
-            <div>
-              <div className={styles.product}>
-                <img src="./backpack.jpg" alt="backpack" />
-                <h2>Backpack</h2>
+        </div>
+        <div className={styles.products}>
+          <p>
+            You are on amazon.com. You can also shop on Amazon Iran for millions
+            of products with fast local delivery. Click here to go to amazon.de
+          </p>
+          <div>
+            <div className={styles.product}>
+              <img src="./backpack.jpg" alt="backpack" />
+              <h2>Backpack</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
               </div>
-              <div className={styles.product}>
-                <img src="./headset.jpg" alt="headset" />
-                <h2>Headset</h2>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
               </div>
-              <div className={styles.product}>
-                <img src="./ski-stick.webp" />
-                <h2>Ski Stick</h2>
+              <a href="#">see more...</a>
+            </div>
+            <div className={styles.product}>
+              <img src="./headset.jpg" alt="headset" />
+              <h2>Headset</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
               </div>
-              <div className={styles.product}>
-                <img src="./laptop.avif" />
-                <h2>Laptop</h2>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
               </div>
-              <div className={styles.product}>
-                <img src="./mobile.avif" />
-                <h2>Mobile</h2>
+              <a href="#">see more...</a>
+            </div>
+            <div className={styles.product}>
+              <img src="./ski-stick.webp" />
+              <h2>Ski Stick</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
               </div>
-              <div className={styles.product}>
-                <img src="./shirt.webp" />
-                <h2>Shirt</h2>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
               </div>
+              <a href="#">see more...</a>
+            </div>
+            <div className={styles.product}>
+              <img src="./laptop.avif" />
+              <h2>Laptop</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
+              </div>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
+              </div>
+              <a href="#">see more...</a>
+            </div>
+            <div className={styles.product}>
+              <img src="./mobile.avif" />
+              <h2>Mobile</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
+              </div>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
+              </div>
+              <a href="#">see more...</a>
+            </div>
+            <div className={styles.product}>
+              <img src="./shirt.webp" />
+              <h2>Shirt</h2>
+              <div className={styles.price}>
+                <span>$</span>
+                <span>99000</span>
+                <span>99</span>
+              </div>
+              <span className={styles.discount}>$1200</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident a aperiam quo unde aspernatur culpa nobis eaque sit
+                ...
+              </p>
+              <div className={styles.stars}>
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <img src="./star.avif" alt="" />
+                <span>1,752</span>
+              </div>
+              <a href="#">see more...</a>
             </div>
           </div>
         </div>
       </main>
-      <footer></footer>
+      <footer className={styles.footer}>
+        <a href="#">Back to top</a>
+        <div>
+          <ul>
+            <li>Get to Know Us</li>
+            <li>Careers</li>
+            <li>Blog</li>
+            <li>About Amazon</li>
+            <li>Investor Relations</li>
+            <li>Amazon Devices</li>
+            <li>Amazon Science</li>
+          </ul>
+          <ul></ul>
+          <ul></ul>
+          <ul></ul>
+        </div>
+      </footer>
     </>
   );
 }
