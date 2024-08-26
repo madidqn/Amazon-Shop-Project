e2bfa90863d99ef207701826901ed5a13fb834aa
mahdieh <madidgn82@gmail.com>
2024-08-26 15:25:03 +0330

    style products

--- src/App.jsx
+++ src/App.jsx
@@ -83,35 +83,64 @@ function App() {
         </nav>
       </header>
       <main className={styles.main}>
-        <Swiper
-          // rewind={true}
-          loop={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Navigation]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="./slide1.avif" alt="slide" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="./slide2.avif" alt="slide" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="./slide3.avif" alt="slide" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="./slide4.avif" alt="slide" />
-          </SwiperSlide>
-        </Swiper>
-        <div className={styles.p}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
-          sapiente eum, temporibus obcaecati optio placeat nesciunt ratione cum
-          suscipit, quos numquam, id ducimus necessitatibus culpa. Fuga odio nam
-          pariatur! Reprehenderit!
+        <div className={styles.slider}>
+          <Swiper
+            // rewind={true}
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src="./slide1.avif" alt="slide" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="./slide2.avif" alt="slide" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="./slide3.avif" alt="slide" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="./slide4.avif" alt="slide" />
+            </SwiperSlide>
+          </Swiper>
+          <div className={styles.products}>
+            <p>
+              You are on amazon.com. You can also shop on Amazon Iran for
+              millions of products with fast local delivery. Click here to go to
+              amazon.de
+            </p>
+            <div>
+              <div className={styles.product}>
+                <img src="./backpack.jpg" alt="backpack" />
+                <h2>Backpack</h2>
+              </div>
+              <div className={styles.product}>
+                <img src="./headset.jpg" alt="headset" />
+                <h2>Headset</h2>
+              </div>
+              <div className={styles.product}>
+                <img src="./ski-stick.webp" />
+                <h2>Ski Stick</h2>
+              </div>
+              <div className={styles.product}>
+                <img src="./laptop.avif" />
+                <h2>Laptop</h2>
+              </div>
+              <div className={styles.product}>
+                <img src="./mobile.avif" />
+                <h2>Mobile</h2>
+              </div>
+              <div className={styles.product}>
+                <img src="./shirt.webp" />
+                <h2>Shirt</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
       <footer></footer>
