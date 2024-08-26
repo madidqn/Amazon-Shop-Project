0005e351daa82710c3511519874e26f59f57f1b2
mahdieh <madidgn82@gmail.com>
2024-08-26 18:02:22 +0330

    component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import Select from "react-select";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 
-//icons
-import { FaMapMarkerAlt, FaSearch, FaBars } from "react-icons/fa";
+// components
+import Header from "./components/header";
 
 // style
 import styles from "./App.module.css";
@@ -12,76 +10,10 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/bundle";
 
-const options = [
-  { value: "all", label: "All" },
-  { value: "mobiles", label: "Mobiles" },
-  { value: "laptops", label: "Laptops" },
-  { value: "mobile accessories", label: "Mobile accessories" },
-];
-
 function App() {
-  const [selectedOption, setSelectedOption] = useState(null);
-  // function ChangeOption(option) {
-  //   setSelectedOption(option);
-  // }
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.searchbar}>
-          <img src="./logo.avif" alt="logo" className={styles.logo} />
-          <div className={styles.deliver}>
-            <FaMapMarkerAlt />
-            <span>
-              <span>Deliver to </span>
-              <span> Iran</span>
-            </span>
-          </div>
-          <div className={styles.inputSearch}>
-            <form action="">
-              <Select
-                value={selectedOption}
-                placeholder="All"
-                onChange={setSelectedOption}
-                options={options}
-              />
-              <input type="text" placeholder="Search Amazon" />
-              <button>
-                <FaSearch className={styles.FaSearch} />
-              </button>
-            </form>
-          </div>
-          <div className={styles.language}>
-            <img src="./language.avif" alt="language" />
-            <span>EN</span>
-          </div>
-          <div className={styles.signIn}>
-            <span>Hello, sign in</span>
-            <span>Account & Lists</span>
-          </div>
-          <div className={styles.signIn}>
-            <span>Returns</span>
-            <span>& Orders</span>
-          </div>
-          <div className={styles.cart}>
-            <span>0</span>
-            <img src="./cart.avif" alt="cart" />
-            <span>Cart</span>
-          </div>
-        </div>
-        <nav className={styles.menu}>
-          <ul>
-            <li>
-              <FaBars className={styles.bergurIcon} />
-              <span>All</span>
-            </li>
-            <li>Today's Deals</li>
-            <li>Customer Service</li>
-            <li>Registry</li>
-            <li>Gift Cards</li>
-            <li>Sell</li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main className={styles.main}>
         <div className={styles.slider}>
           <Swiper
---
+++ src/components/header/index.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import Select from "react-select";
+
+//style
+import styles from "./style.module.css";
+
+//icons
+import { FaMapMarkerAlt, FaSearch, FaBars } from "react-icons/fa";
+
+function Header() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [
+    { value: "all", label: "All" },
+    { value: "mobiles", label: "Mobiles" },
+    { value: "laptops", label: "Laptops" },
+    { value: "mobile accessories", label: "Mobile accessories" },
+  ];
+  return (
+    <header className={styles.header}>
+      <div className={styles.searchbar}>
+        <img src="./logo.avif" alt="logo" className={styles.logo} />
+        <div className={styles.deliver}>
+          <FaMapMarkerAlt />
+          <span>
+            <span>Deliver to </span>
+            <span> Iran</span>
+          </span>
+        </div>
+        <div className={styles.inputSearch}>
+          <form action="">
+            <Select
+              value={selectedOption}
+              placeholder="All"
+              onChange={setSelectedOption}
+              options={options}
+            />
+            <input type="text" placeholder="Search Amazon" />
+            <button>
+              <FaSearch className={styles.FaSearch} />
+            </button>
+          </form>
+        </div>
+        <div className={styles.language}>
+          <img src="./language.avif" alt="language" />
+          <span>EN</span>
+        </div>
+        <div className={styles.signIn}>
+          <span>Hello, sign in</span>
+          <span>Account & Lists</span>
+        </div>
+        <div className={styles.signIn}>
+          <span>Returns</span>
+          <span>& Orders</span>
+        </div>
+        <div className={styles.cart}>
+          <span>0</span>
+          <img src="./cart.avif" alt="cart" />
+          <span>Cart</span>
+        </div>
+      </div>
+      <nav className={styles.menu}>
+        <ul>
+          <li>
+            <FaBars className={styles.bergurIcon} />
+            <span>All</span>
+          </li>
+          <li>Today's Deals</li>
+          <li>Customer Service</li>
+          <li>Registry</li>
+          <li>Gift Cards</li>
+          <li>Sell</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
