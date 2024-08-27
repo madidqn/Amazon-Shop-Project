db7bc9effca063eabcae38408c30855155856162
mahdieh <madidgn82@gmail.com>
2024-08-27 19:50:56 +0330

    added page notfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+// components
 import Header from "./components/header";
 import Home from "./pages/Home";
 import Footer from "./components/footer";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Header />
-      {/* mahsolat */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       {/* mahsol taki */}
       {/* sabad kharid */}
       {/* sign in??? */}
-      {/* not found */}
       <Footer />
     </>
   );
--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -21,24 +21,6 @@ function Products() {
       <div>
         {products.map((product) => (
           <Product product={product} key={product.id} />
-          // <div className={styles.product} key={product.id}>
-          //   <img src={product.src} alt={product.alt} />
-          //   <h2>{product.title}</h2>
-          //   <div className={styles.finalPrice}>
-          //     <span>$</span>
-          //     <span>{product.final_price}</span>
-          //     <span>99</span>
-          //   </div>
-          //   <span className={styles.price}>${product.price}</span>
-          //   <p>{product.discription}</p>
-          //   <div className={styles.stars}>
-          //     {product.rating.map((element, index) => (
-          //       <img src={element} alt="star" key={index} />
-          //     ))}
-          //     <span>{product.voted_number}</span>
-          //   </div>
-          //   <a href="#">see more...</a>
-          // </div>
         ))}
       </div>
     </div>
