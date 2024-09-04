e2ff96223dcbedf3e57fd941e6da4b8f96a68bcc
mahdieh <madidgn82@gmail.com>
2024-09-05 01:06:52 +0330

    style page cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,23 +9,62 @@ import styles from "./Cart.module.css";
 
 function Cart() {
   const { cart } = useSelector((state) => state.products);
+  console.log(cart.length);
   return (
     <Main>
-      <div className={styles.cartContainer}>
-        {cart.lenght > 0 ? (
-          cart.map((product) => <h4 key={product.id}>{product.title}</h4>)
-        ) : (
-          <>
-            <h1>Your Amazon Basket is empty.</h1>
-            <div className={styles.checkProducts}>
-              <p>Check products page for shopping.</p>
-              <Link to={"/"} className={styles.goToHome}>
-                continue shopping
-              </Link>
+      {cart.length > 0 ? (
+        <>
+          <div className={styles.cartContainer}>
+            <div className={styles.product}>
+              <h2>Shopping Basket</h2>
+              <span>Deselect all items</span>
+              <ul>
+                {cart.map((product) => (
+                  <li>
+                    <img src={product.src} alt={product.alt} />
+                    <div>
+                      <div className={styles.title}>
+                        <p>{product.discription}</p>
+                        <h3 key={product.id}>{product.title}</h3>
+                      </div>
+                      <h3>In Stock</h3>
+                      <img src="/prime.avif" alt="prime" />
+                      <div className={styles.gift}>
+                        <input type="checkbox" />
+                        <span>
+                          This will be a gift <a href="#">Learn more</a>
+                        </span>
+                      </div>
+                      <p>
+                        <span>Style Name: </span>0.5 litre work container
+                      </p>
+                      <p>
+                        <span>Colour Name: </span>white
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <h3>Subtotal (44 item): $481200</h3>
             </div>
-          </>
-        )}
-      </div>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias ex
+              debitis quod dolore, voluptatum doloribus eveniet praesentium sunt
+              voluptates impedit. Quisquam eveniet.
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className={styles.cartContainerEmpty}>
+          <h1>Your Amazon Basket is empty.</h1>
+          <div className={styles.checkProducts}>
+            <p>Check products page for shopping.</p>
+            <Link to={"/"} className={styles.goToHome}>
+              continue shopping
+            </Link>
+          </div>
+        </div>
+      )}
     </Main>
   );
 }
