fd63f3a26ad00fdcea4923418d9b544f3a3385a3
mahdieh <madidgn82@gmail.com>
2024-09-04 23:32:55 +0330

    start style page cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,30 @@
-import Main from "../components/main";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+//component
+import Main from "../components/main";
+
+//style
+import styles from "./Cart.module.css";
 
 function Cart() {
   const { cart } = useSelector((state) => state.products);
   return (
     <Main>
-      <div>
-        {cart.map((product) => (
-          <h4 key={product.id}>{product.title}</h4>
-        ))}
+      <div className={styles.cartContainer}>
+        {cart.lenght > 0 ? (
+          cart.map((product) => <h4 key={product.id}>{product.title}</h4>)
+        ) : (
+          <>
+            <h1>Your Amazon Basket is empty.</h1>
+            <div className={styles.checkProducts}>
+              <p>Check products page for shopping.</p>
+              <Link to={"/"} className={styles.goToHome}>
+                continue shopping
+              </Link>
+            </div>
+          </>
+        )}
       </div>
     </Main>
   );
