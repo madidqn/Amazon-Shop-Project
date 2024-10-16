10d6b6c1b52782fe2c88a46f8be880ab89ee5612
mahdieh <madidgn82@gmail.com>
2024-10-16 11:24:21 +0330

    delete package

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-
 //redux
 import { store } from "./store/index.js";
-
 //component
 import App from "./App.jsx";
-
 // styles
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
